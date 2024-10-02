e3112879170628b7bbea3fca646d5f4d2f2e4601
ysksms <ysk06.sss@gmail.com>
2024-10-02 16:24:01 -0700

    add Part1.py

---
+++ Part1.py
@@ -0,0 +1,13 @@
+# Get input from the user
+number = int(input("Enter the number: "))
+n = int(input("Enter the position (from right): "))
+
+# Use integer division and mod to find the nth digit from the right
+nth_digit = (number // 10**(n-1)) % 10
+
+# Print the result
+print(f"The {n}th digit (from the right) is: {nth_digit}")
+
+# References
+# https://medium.com/@alopezmartinez/extracting-digits-from-an-integer-in-python-a-step-by-step-guide-96c9ff98db37
+# https://stackoverflow.com/questions/41321533/how-to-extract-digits-from-a-number-from-left-to-right
